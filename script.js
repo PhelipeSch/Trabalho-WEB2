815f9a37dcf74390eaaa16ad8636a1d925cdc94c
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2025-09-02 11:12:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -219,6 +219,31 @@ function validarEmail() {
   return true;
 }
 
+// Ref: https://www.devmedia.com.br/validar-cpf-com-javascript/23916
+function TestaCPF(cpf) {
+  var Soma;
+  var Resto;
+  Soma = 0;
+  if (cpf == "00000000000") {
+    console.log('aaaaaaaa');
+    return false;
+    
+  }
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
+    return true;
+}
 /*
     Funcao que valida o CPF
     Regras:
@@ -227,31 +252,17 @@ function validarEmail() {
     Deve ter 11 dígitos
 */
 function validarCPF() {
-  let cpf = document.getElementById("CPF");
-
-  // vazio
-  if (cpf.value.length == 0) {
-    cpf.setAttribute("class", "erro");
+  let cpf01 = document.getElementById("CPF").value;
+  let cpf02 = document.getElementById("CPF");
+  if(cpf01.length == 11 && TestaCPF(cpf01)) {
+    // Tudo certo
+    cpf02.setAttribute("class", "correto");
+    return true;
+  } else {
+    // Errado
+    cpf02.setAttribute("class", "erro");
     return false;
   }
-
-  // números
-  for (let i = 0; i < cpf.value.length; i++) {
-    if (!numeros.includes(cpf.value[i])) {
-      cpf.setAttribute("class", "erro");
-      return false;
-    }
-  }
-
-  // 11 dígitos
-  if (cpf.value.length != 11) {
-    cpf.setAttribute("class", "erro");
-    return false;
-  }
-
-  // Tudo certo
-  cpf.setAttribute("class", "correto");
-  return true;
 }
 
 /*
